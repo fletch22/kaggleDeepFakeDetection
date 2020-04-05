49471220344ef40782153385741d916c98699a83
chris@fletch22.com <pluffymunch14432>
2020-04-05 19:50:35 +0000

    enhanced linux install/run

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install build-essential cmake -y
+sudo apt-get install libopenblas-dev liblapack-dev -y
+sudo apt-get install libx11-dev libgtk-3-dev -y
+sudo apt-get install python-dev -y
+sudo apt-get install python3-dev -y
+
+pip install -r ./requirements.txt
